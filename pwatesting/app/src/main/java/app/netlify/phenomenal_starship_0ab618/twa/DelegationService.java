4f7b518595fb462137811084980975990355cf2e
Krish Deshmukh <krishdeshmukh19@gmail.com>
2026-04-28 09:55:28 +0530

    Appupdate

---
+++ pwatesting/app/src/main/java/app/netlify/phenomenal_starship_0ab618/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.netlify.phenomenal_starship_0ab618.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
